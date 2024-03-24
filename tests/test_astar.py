82f2871d7d887554fea93435c52d21207f5d041a
amirhh00 <amirhh00@gmail.com>
2024-03-24 07:33:02 +0000

    init

---
+++ tests/test_astar.py
@@ -0,0 +1,31 @@
+import unittest
+from utils.astar import astar
+
+
+class TestAStar(unittest.TestCase):
+    def test_astar(self):
+        maze = [
+            [0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+            [0, 1, 1, 0, 0, 0, 1, 1, 0, 1],
+            [0, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+            [0, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+            [0, 0, 1, 0, 1, 0, 1, 1, 0, 1],
+            [0, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+            [0, 1, 1, 0, 1, 0, 1, 0, 0, 1],
+            [0, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        ]
+
+        start = (0, 0)
+        end = (9, 9)
+
+        path = astar(maze, start, end)
+        print(path)
+
+        # expect result to exists
+        self.assertIsNotNone(path)
+
+
+if __name__ == "__main__":
+    unittest.main()
